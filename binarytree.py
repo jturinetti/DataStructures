b78cce8cc0cd678543661abca2e3c435d1e79154
jturinetti <jeremy.turinetti@gmail.com>
2014-12-23 10:51:44 -0600

    added breadth first traversal logic, updated print logic, added height/size props

--- binarytree.py
+++ binarytree.py
@@ -2,15 +2,19 @@ __author__ = 'Jeremy Turinetti'
 
 class BinaryTree(object):
     def __init__(self, root_value=None):
+        self.__height = 0
         if root_value is None:
-            self.root = None
+            self.__root = None
+            self.__size = 0
         else:
-            self.root = _Node(root_value)
+            self.__root = _Node(root_value)
+            self.__size = 1
 
     def __insert_node(self, node):
         assert isinstance(node, _Node)
-        cur_node = self.root
+        cur_node = self.__root
         done = False
+        cur_height = 0
         while not done:
             if node.value > cur_node.value:
                 if cur_node.right is None:
@@ -25,30 +29,41 @@ class BinaryTree(object):
                 else:
                     cur_node = cur_node.left
 
+            cur_height = cur_height + 1
+
+        # increase tree height
+        if cur_height > self.__height:
+            self.__height = cur_height
+
+        # increase tree size
+        self.__size = self.__size + 1
+
     def insert_value(self, value):
         node = _Node(value)
-        if self.root is None:
-            self.root = node
+        if self.__root is None:
+            self.__root = node
         else:
             self.__insert_node(node)
 
     def print_tree(self):
-        node = self.root
-        if self.root is None:
+        node = self.__root
+        print("height: ", self.__height)
+        print("size: ", self.__size)
+        if self.__root is None:
             print("Empty binary tree.")
         else:
             self.__print_tree(node)
 
     def __print_tree(self, node):
-        node.status()
+        node.print_node()
         if node.left is not None:
             self.__print_tree(node.left)
         if node.right is not None:
             self.__print_tree(node.right)
 
     def depth_first_traversal(self):
-        node = self.root
-        if self.root is None:
+        node = self.__root
+        if self.__root is None:
             print("Empty binary tree.")
         else:
             self.__depth_first_traversal(node)
@@ -61,15 +76,18 @@ class BinaryTree(object):
         print(node.value)
 
     def breadth_first_traversal(self):
-        node = self.root
-        if self.root is None:
+        node = self.__root
+        if self.__root is None:
             print("Empty binary tree.")
         else:
-            self.__breadth_first_traversal(node)
-
-    def __breadth_first_traversal(self, node):
-        pass  # TODO
-
+            node_queue = [node]
+            while len(node_queue) > 0:
+                cur_node = node_queue.pop(0)
+                print(cur_node.value)
+                if cur_node.left is not None:
+                    node_queue.append(cur_node.left)
+                if cur_node.right is not None:
+                    node_queue.append(cur_node.right)
 
 class _Node(object):
     def __init__(self, value):
@@ -77,13 +95,13 @@ class _Node(object):
         self.left = None
         self.right = None
 
-    def status(self):
+    def print_node(self):
         print("current value: ", self.value)
         if self.left is not None:
             print("  left value: ", self.left.value)
         else:
-            print("  left value: NULL")
+            print("  left value: None")
         if self.right is not None:
             print("  right value: ", self.right.value)
         else:
-            print("  right value: NULL")
+            print("  right value: None")
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from binarytree import BinaryTree
+from random import randint
 __author__ = 'Jeremy Turinetti'
 
 def main():
-    my_tree = BinaryTree(100)
-    my_tree.insert_value(101)
+    # test code here
+    my_tree = BinaryTree()
+    for iter in range(0, 100):
+        num = randint(0, 1000)
+        my_tree.insert_value(num)
+
     my_tree.print_tree()
-
-
 
 main()
